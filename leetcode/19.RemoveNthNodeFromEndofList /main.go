065911fb2ac0e7ee8b4f43e1e68324974be00575
aaronangxz <angxze@gmail.com>
2022-09-29 00:53:38 +0800

    Added 19 comments

--- leetcode/19.RemoveNthNodeFromEndofList /main.go
+++ leetcode/19.RemoveNthNodeFromEndofList /main.go
@@ -5,19 +5,25 @@ import "github.com/aaronangxz/TrainingGround/common"
 func removeNthFromEnd(head *common.Node, n int) *common.Node {
 	fast, slow := head, head
 
+	//fast node will move to the nth position first
 	for n > 0 {
 		fast = fast.Next
 		n--
 	}
 
+	//if fast has reached nil, we need to remove the head
 	if fast == nil {
 		return head.Next
 	}
 
+	//move fast to the end, while moving slow as well
 	for fast.Next != nil {
 		fast = fast.Next
 		slow = slow.Next
 	}
+
+	//slow is at the previous node of the node to be removed
+	//disconnect and link to the next next node
 	slow.Next = slow.Next.Next
 	return head
 }
