d2fe3aa1371b4790080c62d85271f8d577fedde4
aaronangxz <angxze@gmail.com>
2022-11-11 02:48:54 +0800

    Added 1047, 1544

--- leetcode/1047.RemoveAllAdjacentDuplicatesInString/main.go
+++ leetcode/1047.RemoveAllAdjacentDuplicatesInString/main.go
@@ -1,1 +1,38 @@
-package _047_RemoveAllAdjacentDuplicatesInString
+package main
+
+import (
+	"fmt"
+	"github.com/aaronangxz/TrainingGround/common"
+)
+
+func removeDuplicates(s string) string {
+	var st common.Stack
+
+	//Push 1st char onto stack first
+	st.Push(rune(s[0]))
+
+	//Dispose first char
+	s = s[1:]
+
+	for _, ss := range s {
+		//Only if stack is not empty, pop when we found another same char
+		if !st.IsEmpty() && ss == st.Top().(rune) {
+			st.Pop()
+			continue
+		}
+
+		//Otherwise just push onto stack
+		st.Push(ss)
+	}
+
+	out := ""
+	for _, sts := range st {
+		out += string(sts.(rune))
+	}
+	return out
+}
+
+func main() {
+	fmt.Println(removeDuplicates("abbaca"))
+	fmt.Println(removeDuplicates("azxxzy"))
+}
