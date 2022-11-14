607b415435a9937c07285ac136179f7f1510237a
aaronangxz <angxze@gmail.com>
2022-11-14 23:54:46 +0800

    Added 26

---
+++ leetcode/26.RemoveDuplicatesfromSortedArray/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	i := 0
+	for j := range nums {
+		if nums[i] != nums[j] {
+			i += 1
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+
+func main() {
+	fmt.Println(removeDuplicates([]int{1, 1, 2}))
+	fmt.Println(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
+}
