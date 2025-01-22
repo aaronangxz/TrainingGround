9c1e0e5b7607e603b3dfffc5f93216462914b512
aaronangxz <angxze@gmail.com>
2025-01-23 02:24:43 +0800

    Add 1765

---
+++ leetcode/1765.MapOfHighestPeak/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_highestPeak(t *testing.T) {
+	type args struct {
+		isWater [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				isWater: [][]int{{0, 1}, {0, 0}},
+			},
+			want: [][]int{{1, 0}, {2, 1}},
+		},
+		{
+			name: "case 2",
+			args: args{
+				isWater: [][]int{{0, 0, 1}, {1, 0, 0}, {0, 0, 0}},
+			},
+			want: [][]int{{1, 1, 0}, {0, 1, 1}, {1, 2, 2}},
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := highestPeak(tt.args.isWater); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("highestPeak() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
