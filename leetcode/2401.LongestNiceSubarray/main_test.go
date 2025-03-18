4a77add0aace063c96edad9a09065d14d0d66786
aaronangxz <angxze@gmail.com>
2025-03-18 22:42:00 +0800

    Add 2401

---
+++ leetcode/2401.LongestNiceSubarray/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_longestNiceSubarray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				nums: []int{1, 3, 8, 48, 10},
+			},
+			want: 3,
+		},
+		{
+			name: "case 2",
+			args: args{
+				nums: []int{3, 1, 5, 11, 13},
+			},
+			want: 1,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestNiceSubarray(tt.args.nums); got != tt.want {
+				t.Errorf("longestNiceSubarray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
