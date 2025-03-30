186846cc0161ccb3dff766d10fcd64580f92dc32
aaronangxz <angxze@gmail.com>
2025-03-30 12:13:50 +0800

    Add 763

---
+++ leetcode/763.PartitionLabels/main.go
@@ -0,0 +1,21 @@
+package main
+
+func partitionLabels(s string) []int {
+	lastOccurrence := make(map[rune]int)
+	for i, char := range s {
+		lastOccurrence[char] = i
+	}
+
+	result := []int{}
+	start, end := 0, 0
+
+	for i, char := range s {
+		end = max(end, lastOccurrence[char])
+		if i == end {
+			result = append(result, end-start+1)
+			start = i + 1
+		}
+	}
+
+	return result
+}
