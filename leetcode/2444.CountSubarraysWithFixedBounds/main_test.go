db8936680c23d1e53f483e4f22f2bbd720389bfc
aaronangxz <angxze@gmail.com>
2025-04-26 12:09:16 +0800

    Add 2444

---
+++ leetcode/2444.CountSubarraysWithFixedBounds/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func Test_countSubarrays(t *testing.T) {
+	type args struct {
+		nums []int
+		minK int
+		maxK int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "case 1",
+			args: args{
+				nums: []int{1, 3, 5, 2, 7, 5},
+				minK: 1,
+				maxK: 5,
+			},
+			want: 2,
+		},
+		{
+			name: "case 1",
+			args: args{
+				nums: []int{1, 1, 1, 1},
+				minK: 1,
+				maxK: 1,
+			},
+			want: 10,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countSubarrays(tt.args.nums, tt.args.minK, tt.args.maxK); got != tt.want {
+				t.Errorf("countSubarrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
