4be4ed8dc4f3227a05b87a7acc0d0af447206e8e
aaronangxz <angxze@gmail.com>
2025-05-07 21:17:58 +0800

    Add 3341

---
+++ leetcode/3341.FindMinimumTimeToReachLastRoomI/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func Test_minTimeToReach(t *testing.T) {
+	type args struct {
+		moveTime [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				moveTime: [][]int{{0, 4}, {4, 4}},
+			},
+			want: 6,
+		},
+		{
+			name: "case 2",
+			args: args{
+				moveTime: [][]int{{0, 0, 0}, {0, 0, 0}},
+			},
+			want: 3,
+		},
+		{
+			name: "case 3",
+			args: args{
+				moveTime: [][]int{{0, 1}, {1, 2}},
+			},
+			want: 3,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minTimeToReach(tt.args.moveTime); got != tt.want {
+				t.Errorf("minTimeToReach() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
