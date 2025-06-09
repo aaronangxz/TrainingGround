f380955c478a5ef558f2ecc68b67d712b5019673
aaronangxz <angxze@gmail.com>
2025-06-09 22:06:20 +0800

    Add 440

---
+++ leetcode/440.K-thSmallestInLexicographicalOrder/main.go
@@ -0,0 +1,29 @@
+package main
+
+func findKthNumber(n int, k int) int {
+	current := 1
+	k--
+
+	for k > 0 {
+		count := countSteps(n, current, current+1)
+		if count <= k {
+			current++
+			k -= count
+		} else {
+			current *= 10
+			k--
+		}
+	}
+
+	return current
+}
+
+func countSteps(n int, curr int, next int) int {
+	steps := 0
+	for curr <= n {
+		steps += min(n+1, next) - curr
+		curr *= 10
+		next *= 10
+	}
+	return steps
+}
