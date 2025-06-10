dbd5b020dab67bbbb7d11ec38f5378b2884df140
aaronangxz <angxze@gmail.com>
2025-06-10 23:15:19 +0800

    Add 3442

---
+++ leetcode/3442.MaximumDifferenceBetweenEvenAndOddFrequencyI/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_maxDifference(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				s: "aaaaabbc",
+			},
+			want: 3,
+		},
+		{
+			name: "case 2",
+			args: args{
+				s: "abcabcab",
+			},
+			want: 1,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDifference(tt.args.s); got != tt.want {
+				t.Errorf("maxDifference() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
