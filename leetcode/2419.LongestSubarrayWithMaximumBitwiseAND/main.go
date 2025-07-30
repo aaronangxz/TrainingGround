b83f0f86a3de4c4a1b365863a767f50b9b52c0a0
aaronangxz <angxze@gmail.com>
2025-07-30 22:44:31 +0800

    Add 2419

---
+++ leetcode/2419.LongestSubarrayWithMaximumBitwiseAND/main.go
@@ -0,0 +1,26 @@
+package main
+
+func longestSubarray(nums []int) int {
+	maxValue := nums[0]
+	for _, n := range nums {
+		if n > maxValue {
+			maxValue = n
+		}
+	}
+
+	curr := 0
+	maxLengthFound := 0
+
+	for _, n := range nums {
+		if n == maxValue {
+			curr++
+			if curr > maxLengthFound {
+				maxLengthFound = curr
+			}
+		} else {
+			curr = 0
+		}
+	}
+
+	return maxLengthFound
+}
