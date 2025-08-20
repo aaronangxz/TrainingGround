7e79b5f0d16a2bcffddfd36bb4a498e2eb78188e
aaronangxz <angxze@gmail.com>
2025-08-20 23:05:44 +0800

    Add 1277

---
+++ leetcode/1277.CountSquareSubmatricesWithAllOnes/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_countSquares(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				matrix: [][]int{{0, 1, 1, 1}, {1, 1, 1, 1}, {0, 1, 1, 1}},
+			},
+			want: 15,
+		},
+		{
+			name: "case 2",
+			args: args{
+				matrix: [][]int{{1, 0, 1}, {1, 1, 0}, {1, 1, 0}},
+			},
+			want: 7,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countSquares(tt.args.matrix); got != tt.want {
+				t.Errorf("countSquares() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
