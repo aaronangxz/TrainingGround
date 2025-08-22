ec22f6b15d48c42d7ddf10f66fb34ef0103b8b91
aaronangxz <angxze@gmail.com>
2025-08-22 21:42:38 +0800

    Add 3195

---
+++ leetcode/3195.FindTheMinimumAreaToCoverAllnesI/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_minimumArea(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				grid: [][]int{{0, 1, 0}, {1, 0, 1}},
+			},
+			want: 6,
+		},
+		{
+			name: "case 2",
+			args: args{
+				grid: [][]int{{1, 0}, {0, 0}},
+			},
+			want: 1,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumArea(tt.args.grid); got != tt.want {
+				t.Errorf("minimumArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
