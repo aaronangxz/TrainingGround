71eeb4cf566565f88889bcafae520c68f71ba4a9
aaronangxz <angxze@gmail.com>
2025-09-24 23:01:18 +0800

    Add 166

---
+++ leetcode/166.FractionToRecurringDecimal/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func Test_fractionToDecimal(t *testing.T) {
+	type args struct {
+		numerator   int
+		denominator int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "case 1",
+			args: args{
+				numerator:   1,
+				denominator: 2,
+			},
+			want: "0.5",
+		},
+		{
+			name: "case 2",
+			args: args{
+				numerator:   2,
+				denominator: 1,
+			},
+			want: "2",
+		},
+		{
+			name: "case 3",
+			args: args{
+				numerator:   4,
+				denominator: 333,
+			},
+			want: "0.(012)",
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fractionToDecimal(tt.args.numerator, tt.args.denominator); got != tt.want {
+				t.Errorf("fractionToDecimal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
