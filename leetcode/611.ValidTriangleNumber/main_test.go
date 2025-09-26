4160a7f6c5013c9750c4fa027b768b84a9a9ba7c
aaronangxz <angxze@gmail.com>
2025-09-26 23:34:34 +0800

    Add 611

---
+++ leetcode/611.ValidTriangleNumber/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_triangleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				nums: []int{2, 2, 3, 4},
+			},
+			want: 3,
+		},
+		{
+			name: "case 2",
+			args: args{
+				nums: []int{4, 2, 3, 4},
+			},
+			want: 4,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := triangleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("triangleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
