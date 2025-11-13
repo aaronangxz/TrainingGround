60fa53b795128a656d07adbdf6307a3dbc7fb274
aaronangxz <angxze@gmail.com>
2025-11-13 21:47:49 +0800

    Add 3228

---
+++ leetcode/3228.MaximumNumberOfOperationsToMoveOnesToTheEnd/main.go
@@ -0,0 +1,17 @@
+package main
+
+func maxOperations(s string) int {
+	var ans int64 = 0
+	var ones int64 = 0
+	n := len(s)
+	for i := 0; i < n; i++ {
+		if s[i] == '1' {
+			ones++
+		} else {
+			if i > 0 && s[i-1] == '1' {
+				ans += ones
+			}
+		}
+	}
+	return int(ans)
+}
