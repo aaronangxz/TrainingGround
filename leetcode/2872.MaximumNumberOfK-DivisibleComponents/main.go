7452be4d3b14949ddb2f5d6900e5c24a7ea08604
aaronangxz <angxze@gmail.com>
2025-11-28 21:45:39 +0800

    Add 2872

---
+++ leetcode/2872.MaximumNumberOfK-DivisibleComponents/main.go
@@ -0,0 +1,34 @@
+package main
+
+func maxKDivisibleComponents(n int, edges [][]int, values []int, k int) int {
+	adj := make([][]int, n)
+	for _, e := range edges {
+		u, v := e[0], e[1]
+		adj[u] = append(adj[u], v)
+		adj[v] = append(adj[v], u)
+	}
+
+	ans := 0
+
+	var dfs func(u, parent int) int64
+	dfs = func(u, parent int) int64 {
+		sum := int64(values[u] % k)
+
+		for _, v := range adj[u] {
+			if v == parent {
+				continue
+			}
+			childRem := dfs(v, u)
+			sum = (sum + childRem) % int64(k)
+		}
+
+		if sum%int64(k) == 0 {
+			ans++
+			return 0
+		}
+		return sum
+	}
+
+	dfs(0, -1)
+	return ans
+}
