2c3a96db0a438e3784a10536d685c73c2bdd0ad1
aaronangxz <angxze@gmail.com>
2025-12-30 22:47:44 +0800

    Add 840

---
+++ leetcode/840.MagicSquaresInGrid/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_numMagicSquaresInside(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				grid: [][]int{{4, 3, 8, 4}, {9, 5, 1, 9}, {2, 7, 6, 2}},
+			},
+			want: 1,
+		},
+		{
+			name: "case 2",
+			args: args{
+				grid: [][]int{{8}},
+			},
+			want: 0,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numMagicSquaresInside(tt.args.grid); got != tt.want {
+				t.Errorf("numMagicSquaresInside() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
